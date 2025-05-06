1452199c03c47db9f389a7c2287e89befc6ea829
DevanM117 <devanmarion8@gmail.com>
2025-05-06 20:32:28 +0000

    finished database.h

--- Database.h
+++ Database.h
@@ -1,5 +1,21 @@
 //Database.h
+// Database.h
 #ifndef DATABASE_H
-#define DATABASE_H 
+#define DATABASE_H
 
-#include 
+#include "Character.h"
+#include <vector>
+
+class Database {
+private:
+    std::vector<Character> characters;
+
+public:
+    void addCharacter(const Character& character);
+    void listCharacters() const;
+    void searchByAlias(const std::string& alias) const;
+};
+
+#endif // DATABASE_H
+
+
