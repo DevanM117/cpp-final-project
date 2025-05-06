7b605217cec2c8643d011f6db8e3db27067dd394
DevanM117 <devanmarion8@gmail.com>
2025-05-06 20:15:32 +0000

    Added structure to hold character info

--- Character.h
+++ Character.h
@@ -1,1 +1,14 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
 
+#include <string>
+#include <vector>
+
+struct Character {
+    std::string name;
+    std::string alias;
+    std::string affiliations;
+    std::vector<std::string> powers;
+};
+
+#endif // CHARACTER_H
